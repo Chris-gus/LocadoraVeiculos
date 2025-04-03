d7e46ab9bce8e1c44a31875808108fad27248365
Mars Haley <chrisgus2605@gmail.com>
2025-04-02 21:09:47 -0300

    Criado menu inicial e lista de veiculos

--- LocadoraVeiculos/Program.cs
+++ LocadoraVeiculos/Program.cs
@@ -1,49 +1,62 @@
-﻿namespace LocadoraVeiculos
+﻿using System;
+namespace LocadoraVeiculos
 {
     internal class Program
     {
         static void Main(string[] args)
         {
+           List<Veiculo> veiculos = new List<Veiculo>();
+           EscolhaOperacao(veiculos);
+        }
+
+        static void EscolhaOperacao(List<Veiculo> veiculos)
+        {
+            
+            Console.WriteLine("Escolha o que você deseja fazer:");
+            Console.WriteLine("1- Cadastrar novo veículo.");
+            if(veiculos.Count > 0) Console.WriteLine("2- Exibir veículos");
+            Console.WriteLine("0- Sair");
 
         }
-        static void EscolhaTipo(Veiculo v)
+        static void EscolhaTipo(Veiculo v,List<Veiculo> veiculos)
         {
             Console.WriteLine("Que tipo de veículo você deseja? ");
             Console.WriteLine("1 - Carro");
             Console.WriteLine("2 - Moto");
             Console.WriteLine("3 - Caminhão");
-            char op = char.Parse(Console.ReadLine());
+            int op = int.Parse(Console.ReadLine());
 
             switch (op)
             {
-                case (char)1:
-                    Carro cr = new Carro(v.Modelo, v.Marca, v.Ano, v.BaseDiarioAluguel);
-                    InserirInf(cr);
+                case 1:
+                    Veiculo cr = new Carro(v.Modelo, v.Marca, v.Ano, v.BaseDiarioAluguel);
+                    InserirInf();
                     break;
-                case (char)2:
-                    Moto m = new Moto(v.Modelo, v.Marca, v.Ano, v.BaseDiarioAluguel);
-                    InserirInf(m);
+                case 2:
+                    Veiculo m = new Moto(v.Modelo, v.Marca, v.Ano, v.BaseDiarioAluguel);
+                    InserirInf();
                     break;
 
-                case (char)3:
-                    Caminhao cm = new Caminhao(v.Modelo, v.Marca, v.Ano, v.BaseDiarioAluguel);
-                    InserirInf(cm);
+                case 3:
+                    Veiculo cm = new Caminhao(v.Modelo, v.Marca, v.Ano, v.BaseDiarioAluguel);
+                    InserirInf();
                     break;
 
                 default:
                     throw new Exception("Insira um valor válido.");
             }
         }
-        static Veiculo InserirInf(Veiculo v)
+        static Veiculo InserirInf()
         {
             Console.WriteLine("Insira o modelo do veículo: ");
-            v.Modelo = Console.ReadLine();
+            string modelo = Console.ReadLine();
             Console.WriteLine("Insira a marca do veículo: ");
-            v.Marca = Console.ReadLine();
+            string marca = Console.ReadLine();
             Console.WriteLine("Insira o ano do veículo: ");
-            v.Ano = int.Parse(Console.ReadLine());
+            int ano = int.Parse(Console.ReadLine());
             Console.WriteLine("Qual o valor base diário de aluguel desse veículo?");
-            v.BaseDiarioAluguel = double.Parse(Console.ReadLine());
+            double baseDiarioAluguel = double.Parse(Console.ReadLine());
+            Veiculo v = new Veiculo(modelo,marca,ano,baseDiarioAluguel);
             return v;
         }
     }
